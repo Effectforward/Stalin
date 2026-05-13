9da5953c5893d9b444eb58b68cab71a73c527b2e
ofc-mee <optimistic8610@gmail.com>
2026-05-13 22:10:29 +0500

    add

---
+++ spaceship.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "laser.hpp"
+#include "raylib.h"
+#include <vector>
+// Spaceship class
+class Spaceship
+{
+  public:
+	Spaceship();
+	~Spaceship();
+	void Draw();
+	void moveLeft();
+	void moveRight();
+	void Firelaser();
+	std::vector<Laser> lasers; // this will hold all the lasers
+
+  private:
+	int speed = 9;
+	// for image rendering (it is a data structure)
+	Texture2D image;
+	// for poositioning (it is also a data structure)
+	Vector2 position;
+	double lastFireTime;
+};
